d632ed074c1bcb2d92e72be4d2e3e1984922d061
trantu-dev <trananhtu@protonmail.ch>
2020-02-07 02:04:32 +0700

    init express setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// LOAD ENV VAR
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
